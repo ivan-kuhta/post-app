7030152d42eb927231be3057be6e07014627c92e
Ivan <Ivan1998pi22342@ukr.net>
2020-06-30 15:18:21 +0300

    Function Delete Post

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import PostStatusFilter from '../post-status-filter';
@@ -18,24 +18,69 @@ const AppBlock = styled.div`
 //   background-color: gray;  
 // `;
 
-const App = () => {
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data : [
+                { label:"Going to learn React", important: true, id: 1},
+                { label:"That is so good", important: false, id: 2},
+                { label:"I need to break...", important: false, id: 3}
+            ]
+        }
+        this.deleteItem = this.deleteItem.bind(this);
+        this.addItem = this.addItem.bind(this);
 
-    const data = [
-        { label:"Going to learn React", important: true, id: 'qwea' },
-        { label:"That is so good", important: false, id: 'asdasd' },
-        { label:"I need to break...", important: false, id: 'asfas' }
-    ]
-    return (
-        <AppBlock>
-            <AppHeader/>
-            <div className="search-panel d-flex">
-                <SearchPanel/>
-                <PostStatusFilter/>
-            </div>
-            <PostList posts={data}/>
-            <PostAddForm/>
-        </AppBlock>
-    )
+        this.maxId = 4;
+    }
+
+    deleteItem(id) {
+        this.setState(({data})=>{
+            const index = data.findIndex(elem => elem.id === id);
+            // data.splice(index, 1);
+            // return {
+            //     data: data
+            // }
+            const before = data.slice(0, index);
+            const after = data.slice(index + 1);
+
+            const newArray = [...before, ...after];
+
+            return {
+                data: newArray
+            }
+        });
+    }
+
+    addItem(body) {
+        const newItem = {
+            label: body,
+            important: false,
+            id: this.maxId++
+        }
+        this.setState(({data})=>{
+            const newArray = [...data, newItem];
+            return {
+                data: newArray
+            }
+        })
+    }
+
+    render() {
+        return (
+            <AppBlock>
+                <AppHeader/>
+                <div className="search-panel d-flex">
+                    <SearchPanel/>
+                    <PostStatusFilter/>
+                </div>
+                <PostList posts={this.state.data}
+                        onDelete={this.deleteItem} />
+                <PostAddForm
+                        onAdd={this.addItem}
+                />
+            </AppBlock>
+        )
+    }
+
 }
-
-export default App;
--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const PostAddForm = () => {
+const PostAddForm = ({onAdd}) => {
     return (
-        <form className="bottom-panel d-flex">
+        <div className="bottom-panel d-flex">
             <input
                 type="text"
                 placeholder="Про що ви думаєте зараз"
@@ -10,9 +10,11 @@ const PostAddForm = () => {
             />
             <button
                 type="submit"
-                className="btn btn-outline-secondary">
+                className="btn btn-outline-secondary"
+                onClick={() => onAdd('Hello')}
+                >
                 Добавити</button>
-        </form>
+        </div>
     )
 }
 
